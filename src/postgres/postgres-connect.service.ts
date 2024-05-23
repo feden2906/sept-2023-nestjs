ccb43073de674c4d30a99111726b3ff448093342
feden2906 <feden2906@gmail.com>
2024-05-23 23:03:52 +0300

    lesson-5

---
+++ src/postgres/postgres-connect.service.ts
@@ -0,0 +1,32 @@
+import * as path from 'node:path';
+import * as process from 'node:process';
+
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config/dist/config.service';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+import { Config, DatabaseConfig } from '../configs/configs.type';
+
+@Injectable()
+export class PostgresConnectService implements TypeOrmOptionsFactory {
+  constructor(private readonly configService: ConfigService<Config>) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    const databaseConfig = this.configService.get<DatabaseConfig>('database');
+    return {
+      type: 'postgres',
+      host: databaseConfig.host,
+      port: databaseConfig.port,
+      username: databaseConfig.user,
+      password: databaseConfig.password,
+      database: databaseConfig.dbName,
+      entities: [
+        path.join(process.cwd(), 'dist', 'database', 'entities', '*.entity.js'),
+      ],
+      migrations: [
+        path.join(process.cwd(), 'src', 'database', 'migrations', '*.ts'),
+      ],
+      synchronize: false,
+    };
+  }
+}
