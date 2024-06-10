24c062cf7e7275707bbb4bfc1584045e4519d408
feden2906 <feden2906@gmail.com>
2024-06-10 21:36:04 +0300

    project-4

--- src/modules/user/services/user.mapper.ts
+++ src/modules/user/services/user.mapper.ts
@@ -1,3 +1,5 @@
+import * as process from 'node:process';
+
 import { UserEntity } from '../../../database/entities/user.entity';
 import { UserResDto } from '../dto/res/user.res.dto';
 
@@ -8,7 +10,9 @@ export class UserMapper {
       name: user.name,
       email: user.email,
       bio: user.bio || null,
-      image: user.image || null,
+      image: user.image
+        ? `${process.env.AWS_S3_BUCKET_URL}/${user.image}`
+        : null,
       isFollowed: user.followings ? user.followings.length > 0 : false,
     };
   }
